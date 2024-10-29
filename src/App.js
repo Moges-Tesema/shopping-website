49c6127c90f524304f5af9d6c7e4d80cc05a18c8
Moges-Tesema <moges2421@gmail.com>
2024-10-29 14:24:26 -0200

    install react-router-dom and resolve confilict with dependancy and write a couple of react-router code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import HomePage from "./pages/homepage/homepage";
 import { Header } from "./pages/header/Header";
-function App() {
+import { Route, Routes, Link } from "react-router-dom";
+const hatsPages = (props) => (
+  <div>
+    <h1>hats page</h1>
+    <Link to="/">homepage</Link>
+    <br />
+    <Link to="/topics">topics</Link>
+    {console.log(props)}
+    <hr />
+    <button>topics</button>
+  </div>
+);
+const Home = (props) => {
+  console.log("this is property", props);
   return (
-    <>
+    <div>
       <Header />
       <HomePage />
-    </>
+    </div>
+  );
+};
+const TopicsList = () => (
+  <div>
+    <h1>Topics list page</h1>
+  </div>
+);
+const TopicDetail = () => (
+  <div>
+    <h1>Topic detail page</h1>
+  </div>
+);
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/hats" Component={hatsPages} />
+        <Route path="/topics" Component={TopicsList} />
+        <Route path="/tpics/:topicId" Component={TopicDetail} />
+      </Routes>
+    </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
+const HatsPage = () => (
+  <div>
+    <h1>hats Page</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
