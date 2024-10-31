86894935b5ba8bf2bcbe0d98244a3c388a51b59f
Moges-Tesema <moges2421@gmail.com>
2024-10-31 18:03:59 -0200

    install react-route-dom and navigate to different pages with react router components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import HomePage from "./pages/homepage/homepage";
 import { Header } from "./pages/header/Header";
+import { Route, Routes } from "react-router-dom";
+const HomePages = () => <div>welcome to home page</div>;
+const HatPage = () => <div>hats page</div>;
+const CheckOutPage = () => <h1>checkout pages</h1>;
+const Payment = () => <h2>payment page</h2>;
 function App() {
   return (
     <>
-      <Header />
-      <HomePage />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <HomePage />
+            </>
+          }
+        />
+        <Route path="/hats" element={<HatPage />} />
+        <Route path="/checkout" element={<CheckOutPage />} />
+      </Routes>
     </>
   );
 }
--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -1,7 +1,28 @@
-export const Header = () => (
-  <div className="content-center">
-    <h1 className="bg-yellow-600  text-center text-4xl italic font-black py-4">
-      Header
-    </h1>
-  </div>
-);
+import { Link, useNavigate } from "react-router-dom";
+const Pay = () => {
+  useNavigate("/payment");
+};
+export const Header = () => {
+  const navigate = useNavigate();
+  const toPayment = () => {
+    navigate("/payment");
+  };
+  return (
+    <div className="content-center">
+      <h1 className="bg-yellow-600  text-center text-4xl italic font-black py-4">
+        Header
+      </h1>
+      <div className="flex flex-row justify-between">
+        <Link className="bg-green-400 " to="/hats">
+          hat page
+        </Link>
+        <Link className="bg-yellow-300" to="/checkout">
+          checkout page
+        </Link>
+        <h1 className="red" onClick={toPayment}>
+          to payment page
+        </h1>
+      </div>
+    </div>
+  );
+};
